5fe0815f31cae29f35d46a0e0b49458b1510a25a
Danila Platonov <danilaplatonov209@gmail.com>
2025-03-31 15:10:58 +0300

    fix layout

--- src/App.tsx
+++ src/App.tsx
@@ -116,41 +116,6 @@ function App() {
                         </div>
                     </div>
                     <hr style={{ width: "90%" }}></hr>
-                    <div className="content-timings">
-                        <h2>Программа</h2>
-                        <div className="item-test">
-                            <h2 className='numbers'>16.05</h2>
-                            <div className="timing">
-                                <div>
-                                    <span className = "numbers small">15:00</span> Размещение в отеле «PineRiver» (городской округ Сочи, посёлок городского типа 
-                                    Красная Поляна, улица Пчеловодов, 59)
-                                </div>
-                                <div><span className = "numbers small">19:00</span> Ужин в ресторане отеля</div> 
-                            </div>
-                        </div>
-                        <div className="item-test">
-                            <h2 className='numbers'>17.05</h2>
-                            <div className="timings">
-                                <div><span className = "numbers small">08:00 - 09:00</span> Завтрак в отеле «PineRiver»</div>
-                                <div><span className = "numbers small">09:30 - 12:00</span> Конференция 7RL/OKX</div> 
-                                <div><span className = "numbers small">13:00 - 15:00</span> Обед в ресторане «Old Boys»</div>
-                                <div><span className = "numbers small">16:00 - 19:00</span> Банный комплекс «4 стихии»</div> 
-                                <div><span className = "numbers small">21:00 - 22:00</span> Ужин в ресторане «Brunello»</div>
-                                <div><span className = "numbers small">22:00 - 02:00</span> Игра в казино</div> 
-                            </div>
-                        </div>
-                        <div className="item-test">
-                            <h2 className='numbers'>18.05</h2>
-                            <div className="timings">
-                                <div><span className = "numbers small">09:00</span> Завтрак </div>
-                                <div><span className = "numbers small">11:00 - 12:30</span> Купель</div> 
-                                <div><span className = "numbers small">13:00 - 15:00</span> Обед в ресторане «Яблоки печем»</div>
-                                <div><span className = "numbers small">после 15:30</span> Свободное время</div>
-                            </div>
-                        </div>
-                        <div className="content-timings-footer">Выселение доступно до <span className = "numbers small">19.05 12:00</span></div>
-                    </div>
-                    <hr style={{ width: "90%" }}></hr>
                     <div className="content-locations">
                         <div className="content-locations-title">
                             <h2>ЛОКАЦИИ</h2>
@@ -244,6 +209,41 @@ function App() {
                                 а также иметь удобную обувь, так как местность гористая
                             </div>
                         </div>
+                    </div>
+                    <hr style={{ width: "90%" }}></hr>
+                    <div className="content-timings">
+                        <h2>Программа</h2>
+                        <div className="item-test">
+                            <h2 className='numbers'>16.05</h2>
+                            <div className="timing">
+                                <div>
+                                    <span className = "numbers small">15:00</span> Размещение в отеле «PineRiver» (городской округ Сочи, посёлок городского типа 
+                                    Красная Поляна, улица Пчеловодов, 59)
+                                </div>
+                                <div><span className = "numbers small">19:00</span> Ужин в ресторане отеля</div> 
+                            </div>
+                        </div>
+                        <div className="item-test">
+                            <h2 className='numbers'>17.05</h2>
+                            <div className="timings">
+                                <div><span className = "numbers small">08:00 - 09:00</span> Завтрак в отеле «PineRiver»</div>
+                                <div><span className = "numbers small">09:30 - 12:00</span> Конференция 7RL/OKX</div> 
+                                <div><span className = "numbers small">13:00 - 15:00</span> Обед в ресторане «Old Boys»</div>
+                                <div><span className = "numbers small">16:00 - 19:00</span> Банный комплекс «4 стихии»</div> 
+                                <div><span className = "numbers small">21:00 - 22:00</span> Ужин в ресторане «Brunello»</div>
+                                <div><span className = "numbers small">22:00 - 02:00</span> Игра в казино</div> 
+                            </div>
+                        </div>
+                        <div className="item-test">
+                            <h2 className='numbers'>18.05</h2>
+                            <div className="timings">
+                                <div><span className = "numbers small">09:00</span> Завтрак </div>
+                                <div><span className = "numbers small">11:00 - 12:30</span> Купель</div> 
+                                <div><span className = "numbers small">13:00 - 15:00</span> Обед в ресторане «Яблоки печем»</div>
+                                <div><span className = "numbers small">после 15:30</span> Свободное время</div>
+                            </div>
+                        </div>
+                        <div className="content-timings-footer">Выселение доступно до <span className = "numbers small">19.05 12:00</span></div>
                     </div>
                     <div className="RSVP">
                         <div className="title">ПРОСЬБА ПОДТВЕРДИТЬ СВОЕ ПРИСУТСТВИЕ</div>
