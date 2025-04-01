1ea648084e370e55a2c9db1d1ea9a293772be496
Danila Platonov <danilaplatonov209@gmail.com>
2025-04-01 11:05:21 +0300

    add user-table

---
+++ src/components/partials/users-table.tsx
@@ -0,0 +1,71 @@
+import { useEffect, useState } from 'react';
+import { Table } from 'antd';
+import { useParams } from 'react-router';
+import axios from 'axios';
+import './users-table.scss'; 
+
+
+const UsersTable = () => {
+    
+    const [users, setUsers] = useState([]);
+    const params = useParams();
+
+    useEffect(() => {
+        axios.post("http://localhost:5003/users", { password: params.pass })
+        .then((res) => {
+            setUsers(res.data.users);
+        })
+        .catch((error) => {
+            console.error(error);
+        })
+    }, []);
+    
+    const dataSource = users.map((user: any, index: number) => {
+        return {
+            ...user,
+            key: index
+        }
+    });
+      
+    const columns = [
+        {
+            title: 'Фио',
+            dataIndex: 'fio',
+            key: 'fio',
+        },
+        {
+            title: 'Номер',
+            dataIndex: 'number',
+            key: 'number',
+        },
+        {
+            title: 'Статус',
+            dataIndex: 'status',
+            key: 'status',
+            render: (status: boolean) => 
+                <div 
+                    style={{ color: status ? "green" : "red" }}
+                >
+                        { status ? "Учавствует" : "Не учавствует" }
+                </div>,
+        }
+    ];
+      
+
+    return (
+        <div 
+            className="users-table"
+        >
+            <Table 
+                scroll={{ y: 500 }} 
+                className='resized' 
+                pagination={{ pageSize: 10}} 
+                dataSource={dataSource} 
+                columns={columns}
+            >
+            </Table>
+        </div>
+    )
+};
+
+export default UsersTable;
--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,20 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { BrowserRouter } from 'react-router'
+import { BrowserRouter, Route, Routes } from 'react-router'
+import UsersTable from './components/partials/users-table.tsx'
+import Main from './components/partials/main.tsx'
 import App from './App.tsx'
 import './index.css'
+
 
 createRoot(document.getElementById('root')!).render(
     <BrowserRouter>
       <StrictMode>
+        <Routes>
+          <Route path='/' element={<Main />}></Route>
+          <Route path='/users/info/:pass' element={<UsersTable />}></Route>
+        </Routes>
         <App />
-      </StrictMode>,
+      </StrictMode>
     </BrowserRouter>
 )
